9c12ecf45458e3acce16b1d7008f5e3fdfbc5c1f
aneurysmjs <aneurysmjs@outlook.com>
2019-02-16 22:47:54 +0300

    feat(Admin/Products): fetch products from store

--- src/pages/Admin/Products/Products.vue
+++ src/pages/Admin/Products/Products.vue
@@ -1,13 +1,25 @@
 <template>
-  <h3>{{ title }}</h3>
+  <section>
+    <h3>{{ title }}</h3>
+    {{ products }}
+  </section>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   data() {
     return {
       title: 'Products!!!',
     };
+  },
+  computed: mapState(['products']),
+  created() {
+    this.fetchProducts();
+  },
+  methods: {
+    ...mapActions(['fetchProducts']),
   },
 };
 </script>

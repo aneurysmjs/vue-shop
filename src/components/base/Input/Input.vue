d47112340dc1deecb766c7bb373fc4f3efcd05cc
aneurysmjs <aneurysmjs@outlook.com>
2019-02-18 23:17:53 +0300

    feat(components/base): add Input

---
+++ src/components/base/Input/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <input
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type :[String | Number],
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .input {
+    
+  }
+</style>

11fa6c61d1c5aee5cc6884b1fda62b1b7e34dd15
jero <jeroflat@gmail.com>
2019-03-18 00:31:05 +0300

    feat(store/users): avoid unnecessary request
    add getters and check if the users already exist in the store

--- src/store/modules/users/actions.js
+++ src/store/modules/users/actions.js
@@ -2,13 +2,18 @@ import api from 'api';
 import * as types from './actionTypes';
 
 export default {
-  async fetchUsers({ commit }) {
-    commit(types.USERS_REQUEST);
-    try {
-      const response = await api.get('/users');
-      commit(types.USERS_SUCCESS, response.data);
-    } catch (error) {
-      commit(types.USERS_FAILURE, error);
+  async fetchUsers({ commit, getters }) {
+    const users = getters.getUsers();
+    if (users.length > 0) {
+      commit(types.USERS_SUCCESS, users);
+    } else {
+      commit(types.USERS_REQUEST);
+      try {
+        const response = await api.get('/users');
+        commit(types.USERS_SUCCESS, response.data);
+      } catch (error) {
+        commit(types.USERS_FAILURE, error);
+      }
     }
   },
 };

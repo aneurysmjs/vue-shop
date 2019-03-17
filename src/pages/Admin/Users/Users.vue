16cc9e8cde3650d0f8242e03d356c42f476d8e54
jero <jeroflat@gmail.com>
2019-03-17 21:20:49 +0300

    feat(admin/users): set API_URL with api's enpoint
    fetch users

--- src/pages/Admin/Users/Users.vue
+++ src/pages/Admin/Users/Users.vue
@@ -1,10 +1,26 @@
 <template>
-  <h2>Users</h2>
+  <section>
+    <h2>Users</h2>
+    <pre>{{ users }}</pre>
+  </section>
 </template>
 
 <script>
+import { createNamespacedHelpers } from 'vuex';
+
+const { mapState, mapActions } = createNamespacedHelpers('users');
+
 export default {
   name: 'Users',
+  computed: {
+    ...mapState(['users']),
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    ...mapActions(['fetchUsers']),
+  },
 };
 </script>
 
--- src/store/modules/users/actions.js
+++ src/store/modules/users/actions.js
@@ -5,7 +5,7 @@ export default {
   async fetchUsers({ commit }) {
     commit(types.USERS_REQUEST);
     try {
-      const response = api.get('/users');
+      const response = await api.get('/users');
       commit(types.USERS_SUCCESS, response.data);
     } catch (error) {
       commit(types.USERS_FAILURE, error);

85162aafa3b03be46a4b5671e7745d283e2b469d
aneurysm <aneurysmjs@outlook.com>
2022-10-11 01:45:29 +0200

    refactor(i18n): use new creation syntax

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,8 @@
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
+import { createI18n } from 'vue-i18n';
 
-import { LANG } from 'constants';
+import { LANG } from '@/constants';
 
-Vue.use(VueI18n);
-
-const i18n = new VueI18n({
+const i18n = createI18n({
   locale: LANG,
 });
 

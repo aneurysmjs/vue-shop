30ade4b3e58670f0eaa063ed285ff3a648bc5559
aneurysm <aneurysmjs@outlook.com>
2022-10-11 01:11:29 +0200

    refactor(main): remove vuex

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from '@/components/core/App.vue';
-import store from '@/store';
 import { router } from './router';
+
+import './assets/css/styles.css';
 
 const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).use(store)
+createApp(App).use(router).use(pinia)
   .mount('#app');

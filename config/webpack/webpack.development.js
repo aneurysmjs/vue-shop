b6330a1853b03f6d39dee98fcf035fdb8a14dc3a
aneurysm <aneurysmjs@outlook.com>
2022-10-15 20:22:36 +0200

    build(webpack.development): fix publicPath enpoint
    add trailing '/' for properly serving files

--- config/webpack/webpack.development.js
+++ config/webpack/webpack.development.js
@@ -5,7 +5,6 @@ const { DEVSERVER_HOST, PORT } = require('../const');
 const paths = require('../paths');
 
 module.exports = (env) => {
-  // eslint-disable-next-line global-require
   const commonConfig = require('./webpack.common')(env);
 
   /** @type {import('webpack').Configuration} */
@@ -14,28 +13,13 @@ module.exports = (env) => {
 
     output: {
       path: paths.src,
-      publicPath: `${DEVSERVER_HOST}:${PORT}`,
+      publicPath: `${DEVSERVER_HOST}:${PORT}/`,
       filename: '[name].bundle.js',
       sourceMapFilename: '[name].map',
     },
 
-    devServer: {
-      static: paths.src,
-      compress: true,
-      port: 9000,
-      /**
-       * Basically tells the dev-server "hey! if you don't match something here,
-       * the browser probable would know what to do with it"
-       */
-      historyApiFallback: true,
-      open: true,
-      client: {
-        overlay: {
-          warnings: false,
-          errors: false,
-        },
-      },
-    },
+    // `devServer`is not longer needed since we're using webpack-dev-middleware that
+    // has more lower level api
     plugins: [new webpack.HotModuleReplacementPlugin()],
   });
 };

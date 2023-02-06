00fa05ee5c0e3decd737fb53e3fbbdbe5a5e683f
aneurysm <aneurysmjs@outlook.com>
2023-02-06 15:21:16 +0100

    feat(fontawesomeicon): add faBorderAll and faGrip

--- src/components/base/FontAwesomeIcon/FontAwesomeIcon.ts
+++ src/components/base/FontAwesomeIcon/FontAwesomeIcon.ts
@@ -1,7 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons/faCartShopping';
+import { faBorderAll } from '@fortawesome/free-solid-svg-icons/faBorderAll';
+import { faGrip } from '@fortawesome/free-solid-svg-icons/faGrip';
 
 library.add(faCartShopping);
+library.add(faBorderAll);
+library.add(faGrip);
 
 export default FontAwesomeIcon;
